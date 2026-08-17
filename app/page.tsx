d88e2c95e6c45efa065c1add688605331f474ab0
Syed <mirulkimi226@gmail.com>
2026-08-18 00:38:49 +0800

    try conflict

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,8 @@ export default function Home() {
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the{" "}
             <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              SIYES ARRR LIP nate alip lipak / saja do , mari ber unicorn
+              SIYES ARRR LIP nate alip lipak / saja do , mari ber unicorn/askjdhasdhjkfgajksdhfgashjkdfg
+              /nate alip
             </code>{" "}
             file.
           </h1>
